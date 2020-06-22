5a8d7c80968c7abf78b5ee706c2791778afb4e4c
BeokBeok <kekemusa37@nate.com>
2020-06-22 23:36:19 +0900

    [추가] token verify function

--- src/main/kotlin/com/example/parayo/domain/auth/JWTUtil.kt
+++ src/main/kotlin/com/example/parayo/domain/auth/JWTUtil.kt
@@ -2,6 +2,7 @@ package com.example.parayo.domain.auth
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.interfaces.DecodedJWT
 import java.util.*
 
 object JWTUtil {
@@ -40,4 +41,19 @@ object JWTUtil {
             .withClaim(EMAIL, email)
             .sign(refreshAlgorith)
     }
+
+    fun verify(token: String): DecodedJWT =
+        JWT.require(algorithm)
+            .withIssuer(ISSUER)
+            .build()
+            .verify(token)
+
+    fun verifyRefresh(token: String): DecodedJWT =
+        JWT.require(refreshAlgorith)
+            .withIssuer(ISSUER)
+            .build()
+            .verify(token)
+
+    fun extractEmail(jwt: DecodedJWT): String =
+        jwt.getClaim(EMAIL).asString()
 }
