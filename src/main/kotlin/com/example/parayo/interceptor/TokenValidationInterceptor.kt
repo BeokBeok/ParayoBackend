bcc8b38e92dd32f47e2b7ea86baffd741d747ca6
BeokBeok <kekemusa37@nate.com>
2020-06-23 00:09:00 +0900

    [추가] pre and post handle function

--- src/main/kotlin/com/example/parayo/interceptor/TokenValidationInterceptor.kt
+++ src/main/kotlin/com/example/parayo/interceptor/TokenValidationInterceptor.kt
@@ -1,10 +1,14 @@
 package com.example.parayo.interceptor
 
+import com.example.parayo.domain.auth.JWTUtil
 import com.example.parayo.domain.auth.UserContextHolder
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.ModelAndView
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @Component
 class TokenValidationInterceptor @Autowired constructor(
@@ -13,10 +17,50 @@ class TokenValidationInterceptor @Autowired constructor(
 
     private val logger = LoggerFactory.getLogger(this::class.java)
 
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+        val authHeader = request.getHeader(AUTHORIZATION)
+
+        if (authHeader.isNullOrBlank()) {
+            val methodToServletPath = request.method to request.servletPath
+            if (!DEFAULT_ALLOWED_API_URLS.contains(methodToServletPath)) {
+                response.sendError(401)
+                return false
+            }
+            return true
+        } else {
+            val grantType = request.getParameter("grant_type")
+            val token = extractToken(authHeader)
+            return handleToken(grantType, token, response)
+        }
+    }
+
+    override fun postHandle(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        modelAndView: ModelAndView?
+    ) = userContextHolder.clear()
+
+    private fun handleToken(grantType: String, token: String, response: HttpServletResponse) = try {
+        val jwt = when (grantType) {
+            GRANT_TYPE_REFRESH -> JWTUtil.verifyRefresh(token)
+            else -> JWTUtil.verify(token)
+        }
+        val email = JWTUtil.extractEmail(jwt)
+        userContextHolder.set(email)
+        true
+    } catch (e: Exception) {
+        logger.error("토큰 검증 실패. token = $token", e)
+        response.sendError(401)
+        false
+    }
+
+    private fun extractToken(token: String) =
+        token.replace(BEARER, "").trim()
+
     companion object {
-        private const val Authorization = "Authorization"
+        private const val AUTHORIZATION = "Authorization"
         private const val BEARER = "Bearer"
-        private const val GRANT_TYPE = "grant_type"
         private const val GRANT_TYPE_REFRESH = "refresh_token"
 
         // TODO Spring Security 를 사용하여 좀 더 세련된 방법으로 설정
