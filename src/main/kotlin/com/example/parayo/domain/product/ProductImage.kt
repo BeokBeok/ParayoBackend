efd19dba2dc31204499b35d76f74ef8cb4edbacb
BeokBeok <kekemusa37@nate.com>
2020-06-22 22:10:37 +0900

    [추가] ProductImage Entity

---
+++ src/main/kotlin/com/example/parayo/domain/product/ProductImage.kt
@@ -0,0 +1,29 @@
+package com.example.parayo.domain.product
+
+import java.util.*
+import javax.persistence.*
+
+@Entity(name = "product_image")
+class ProductImage(
+    private val path: String,
+    private val productId: Long = 0L
+) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L
+
+    var createdAt: Date = Date(0)
+    var updatedAt: Date = Date(0)
+
+    @PrePersist
+    fun prePersist() {
+        createdAt = Date()
+        updatedAt = Date()
+    }
+
+    @PreUpdate
+    fun preUpdate() {
+        updatedAt = Date()
+    }
+}
