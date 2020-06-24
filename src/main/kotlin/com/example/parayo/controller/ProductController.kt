4a252849cb079e604302608ccc95f984e95ba65e
BeokBeok <kekemusa37@nate.com>
2020-06-24 22:50:44 +0900

    [추가] 상품 등록 함수

--- src/main/kotlin/com/example/parayo/controller/ProductController.kt
+++ src/main/kotlin/com/example/parayo/controller/ProductController.kt
@@ -2,8 +2,11 @@ package com.example.parayo.controller
 
 import com.example.parayo.common.ApiResponse
 import com.example.parayo.domain.product.registration.ProductImageService
+import com.example.parayo.domain.product.registration.ProductRegistrationRequest
+import com.example.parayo.domain.product.registration.ProductRegistrationService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
@@ -11,10 +14,15 @@ import org.springframework.web.multipart.MultipartFile
 @RestController
 @RequestMapping("/api/v1")
 class ProductController @Autowired constructor(
-    private val productImageService: ProductImageService
+    private val productImageService: ProductImageService,
+    private val productRegistrationService: ProductRegistrationService
 ) {
 
     @PostMapping("/product_images")
     fun uploadImage(image: MultipartFile) =
         ApiResponse.ok(productImageService.uploadImage(image))
+
+    @PostMapping("/products")
+    fun register(@RequestBody request: ProductRegistrationRequest) =
+        ApiResponse.ok(productRegistrationService.register(request))
 }
