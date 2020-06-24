964649befe57c18a0d4b8438a529c28e63373d6a
BeokBeok <kekemusa37@nate.com>
2020-06-24 22:48:12 +0900

    [추가] ProductRegistrationRequest 에 무결섬 검증 함수

--- src/main/kotlin/com/example/parayo/domain/product/registration/ProductRegistrationRequest.kt
+++ src/main/kotlin/com/example/parayo/domain/product/registration/ProductRegistrationRequest.kt
@@ -1,9 +1,34 @@
 package com.example.parayo.domain.product.registration
 
+import com.example.parayo.common.ParayoException
+import com.example.parayo.domain.product.Product
+import com.example.parayo.domain.product.ProductImage
+import com.example.parayo.domain.product.ProductStatus
+
 data class ProductRegistrationRequest(
     val name: String,
     val description: String,
     val price: Int,
     val categoryId: Int,
     val imageIds: List<Long>
-)
+) {
+
+    fun validateRequest() = when {
+        name.length !in 1..40 ||
+                imageIds.size !in 1..4 ||
+                imageIds.isEmpty() ||
+                description.length !in 1..500 ||
+                price <= 0 -> throw ParayoException("올바르지 않은 상품 정보입니다.")
+        else -> Unit
+    }
+
+    fun toProduct(images: List<ProductImage>, userId: Long) = Product(
+        name = name,
+        description = description,
+        price = price,
+        categoryId = categoryId,
+        status = ProductStatus.SELLABLE,
+        images = images,
+        userId = userId
+    )
+}
